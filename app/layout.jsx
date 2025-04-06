f8c313b0d73ff55ddef3ef328694847355037328
Carlos Andrés Chiale <carlos@thechfactory.com>
2025-04-06 19:48:40 -0300

    Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Geocodificadores Uruguay",
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
